88cc7925a9606ead2816621f4c95ee73eb1a69fa
Maria Gabrielly <ggabrielly.ggaby@gmail.com>
2022-07-05 19:04:42 -0300

    Iniciando o projeto

---
+++ services/firebase.js
@@ -0,0 +1,17 @@
+
+const firebase = require('firebase/compact/app');
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    projectID: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.FIREBASE_APPID,
+}; 
+
+//Inicializar App Firebase
+const app = firebase.initializeApp(firebaseConfig);
+console.log(app);
+
+module.exports = app;
---
+++ views/app.js
@@ -0,0 +1,5 @@
+// Importação CommonJS modules
+require('dotenv').config();
+const firebase = require('../services/firebase');
+
+console.log(firebase);
