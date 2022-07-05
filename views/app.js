fe2e5ca7b5582d7bed38b96321bbc7ce73b1c10a
Maria Gabrielly <ggabrielly.ggaby@gmail.com>
2022-07-05 20:50:28 -0300

    Atualizando

--- views/app.js
+++ views/app.js
@@ -1,5 +1,18 @@
-// Importação CommonJS modules
+// Importação CommonJS modules - Firebase
 require('dotenv').config();
 const firebase = require('../services/firebase');
-
 console.log(firebase);
+
+// Importação CommonJS modules
+const express = require('express');
+
+// Criar aplicação WEB express
+const app = express();
+// Rotas (End Points)
+app.get('/', (req, res) => {
+  // console.log('Rota / foi chamada.');
+  res.render('login');
+});
+
+// Exportar a aplicação
+module.exports = app;
